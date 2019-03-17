f8e39f60507b3692f026b50f07afe879830e4fb0
Brian Liotti <Brian Liotti>
2019-03-17 07:03:32 -0400

    sql

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-
 app = Flask(__name__)
 
+
 app.config['SECRET_KEY'] = '7443020c844bdca9a819bff92ba31228'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
 
 posts = [
     {
